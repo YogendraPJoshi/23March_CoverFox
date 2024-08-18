aa2585fb9e3904f3badec6ebd3dd22a0d2bf1a49
YogendraPJoshi <yogendraJoshi02@gmail.com>
2024-08-18 07:54:28 +0530

    Yogendra[New Feature Added]

---
+++ src/test/java/coverFoxTest/COV_TC_123_345_AgeValidation_GenderValidation.java
@@ -0,0 +1,16 @@
+package coverFoxTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class COV_TC_123_345_AgeValidation_GenderValidation {
+  @Test
+  public void ageValidation() {
+	  Reporter.log("new feature added", true);
+  }
+  
+  @Test
+  public void genderValidation() {
+	  Reporter.log("new feature added", true);
+  }
+}
